c0b538057ce7fdc86f439f47d7d5e911429774c0
ddanielaiwuyo <aiwuyoeghosadaniel2@gmail.com>
2026-03-30 15:06:53 +0100

    sharing props with use state

--- use_state/src/Counter.test.jsx
+++ use_state/src/Counter.test.jsx
@@ -1,29 +1,29 @@
-import { render, screen } from "@testing-library/react";
-import userEvent from "@testing-library/user-event";
-import Counter from "./components/Counter";
-
-test("renders with inital value 0", () => {
-	render(<Counter />)
-
-	expect(screen.getByRole("heading")).toHaveTextContent("0")
-})
-
-test("decreases value by 1 when decrement button is clicked", async () => {
-	render(<Counter />)
-	await userEvent.click(screen.getByText(/Decrement/i))
-
-	expect(screen.getByRole("heading")).toHaveTextContent("-1")
-})
-
-
-test("renders with a new value of two , when Increment button is clicked twice", async () => {
-	render(<Counter />)
-	const incrementBtn = screen.getByText(/Increment/i)
-	await userEvent.click(incrementBtn)
-	await userEvent.click(incrementBtn)
-
-
-	const counterVal = screen.getByRole("heading")
-	console.log("COUNTER -> ", counterVal.innerHTML)
-	expect(counterVal).toHaveTextContent("2")
-})
+// import { render, screen } from "@testing-library/react";
+// import userEvent from "@testing-library/user-event";
+// import Counter from "./components/Counter";
+//
+// test("renders with inital value 0", () => {
+// 	render(<Counter />)
+//
+// 	expect(screen.getByRole("heading")).toHaveTextContent("0")
+// })
+//
+// test("decreases value by 1 when decrement button is clicked", async () => {
+// 	render(<Counter />)
+// 	await userEvent.click(screen.getByText(/Decrement/i))
+//
+// 	expect(screen.getByRole("heading")).toHaveTextContent("-1")
+// })
+//
+//
+// test("renders with a new value of two , when Increment button is clicked twice", async () => {
+// 	render(<Counter />)
+// 	const incrementBtn = screen.getByText(/Increment/i)
+// 	await userEvent.click(incrementBtn)
+// 	await userEvent.click(incrementBtn)
+//
+//
+// 	const counterVal = screen.getByRole("heading")
+// 	console.log("COUNTER -> ", counterVal.innerHTML)
+// 	expect(counterVal).toHaveTextContent("2")
+// })
--- use_state/src/components/Counter.jsx
+++ use_state/src/components/Counter.jsx
@@ -1,18 +1,53 @@
+import { useState } from "react"
 
-import { useState } from "react";
-
-function Counter() {
-	const [count, setValue] = useState(0)
-
-	const increment = () => setValue(count + 1);
-	const decrement = () => setValue(count - 1);
-	return (
-		<>
-			<h1>Count {count} </h1>
-			<button onClick={increment}> Increment </button>
-			<button onClick={decrement}> Decrement </button>
-		</>
-	)
+function CountDisplay(props) {
+	return <h1>Count Value :: {props.count}</h1>
 }
 
+function IncreaseButton(props) {
+	const incrementCounter = () => {
+		props.setCount(props.count + 1)
+	}
+
+	return <button onClick={incrementCounter}>Increment Counter</button>
+}
+
+function DecreaseButton(props) {
+	const reduce = () => {
+		props.setCount(props.count - 1)
+	}
+
+	return <button onClick={reduce}>Decrement Counter</button>
+}
+
+
+function Counter(props) {
+	const [count, setCount] = useState(0)
+	return (
+		<div>
+			<CountDisplay count={count} />
+			<IncreaseButton setCount={setCount} count={count} />
+			<DecreaseButton setCount={setCount} count={count} />
+		</div>
+	)
+}
 export default Counter
+
+//
+// import { useState } from "react";
+//
+// function Counter() {
+// 	const [count, setValue] = useState(0)
+//
+// 	const increment = () => setValue(count + 1);
+// 	const decrement = () => setValue(count - 1);
+// 	return (
+// 		<>
+// 			<h1>Count {count} </h1>
+// 			<button onClick={increment}> Increment </button>
+// 			<button onClick={decrement}> Decrement </button>
+// 		</>
+// 	)
+// }
+//
+// export default Counter
