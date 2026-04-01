2df975811b4ae65894162cddc0b5ca1dc5a73d9b
ddanielaiwuyo <aiwuyoeghosadaniel2@gmail.com>
2026-04-01 15:19:18 +0100

    completed 'forms_in_react -> https://journey.makers.tech/pages/effects-and-fetch'

---
+++ forms/src/App.jsx
@@ -0,0 +1,13 @@
+import Form from "./components/Form";
+import makersLogo from "./assets/Makers-Logo.png";
+import "./App.css";
+
+function App() {
+	return (
+		<>
+			<Form />
+		</>
+	);
+}
+
+export default App;
